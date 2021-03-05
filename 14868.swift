e8454f278fcec4217b59be4f0ef112f4d3c0c406
yabby <yabby1997@gmail.com>
2021-03-06 00:34:24 +0900

    14868 Swift로 메모리초과

--- 14868.swift
+++ 14868.swift
@@ -1,35 +1,43 @@
 import Foundation
 
-struct Area {
+class Area {
     var x: Int
     var y: Int
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
 }
 
 struct Queue<T> {
     var list: [T]
-    var from: Int
-    var to: Int
     
     func size() -> Int {
-        return to - from
+        return list.count
     }
     
     func isEmpty() -> Bool {
         return size() == 0
     }
     
+    func peak() -> T? {
+        if isEmpty() {
+            return nil
+        } else {
+            return list[0]
+        }
+    }
+    
     mutating func enqueue(_ element: T) {
         list.append(element)
-        to += 1
     }
     
     mutating func dequeue() -> T? {
         if isEmpty() {
             return nil
         } else {
-            let target = list[from]
-            from += 1
-            return target
+            return list.removeFirst()
         }
     }
 }
@@ -39,38 +47,41 @@ let yDirections = [0, 0, 1, -1]
 
 let input = readLine()!.split(separator: " ").map({Int(String($0))!})
 let width = input[0]
-let numOfGivenCivilizations = input[1]
-var numOfCurrentCivilizations = numOfGivenCivilizations
-var civilizations = Array(repeating: Queue<Area>(list: [], from: 0, to: 0), count: numOfGivenCivilizations)
-var civilizationMap = Array(repeating: Array(repeating: -1, count: width + 1), count: width + 1)
-var civilizationsMergedTo = Array(repeating: -1, count: numOfGivenCivilizations)
-var takenTurns = 0
+let numOfCivs = input[1]
+var remainingCivs = numOfCivs
+var civQueue = Array(repeating: Queue<Area>(list: []), count: numOfCivs)
+var map = Array(repeating: Array(repeating: -1, count: width + 1), count: width + 1)
+var civGroups = Array(repeating: -1, count: numOfCivs)
+var turns = 0
+var numOfTry = 0
+var currentTry = 0
 
-for civilizationNumber in 0..<numOfGivenCivilizations {
+for civilizationNumber in 0..<numOfCivs {
     let civilizationInfo = readLine()!.split(separator: " ").map({Int(String($0))!})
     let x = civilizationInfo[0]
     let y = civilizationInfo[1]
-    civilizations[civilizationNumber].enqueue(Area(x: x, y: y))
-    civilizationMap[x][y] = civilizationNumber
-    civilizationsMergedTo[civilizationNumber] = civilizationNumber
+    civQueue[civilizationNumber].enqueue(Area(x: x, y: y))
+    map[x][y] = civilizationNumber
+    civGroups[civilizationNumber] = civilizationNumber
 }
 
-while numOfCurrentCivilizations != 1 {
-    for number in 0..<numOfGivenCivilizations {
+while remainingCivs != 1 {
+    for number in 0..<numOfCivs {
         BFS(civilization : number)
     }
-    takenTurns += 1
+    turns += 1
 }
 
-print(takenTurns)
+print(turns)
 
 
 func BFS(civilization number: Int) {
-    var numOfTry = civilizations[number].size()
-    var currentTry = 0
+    numOfTry = civQueue[number].size()
+    currentTry = 0
     while currentTry < numOfTry {
         currentTry += 1
-        let currentArea = civilizations[number].dequeue()!
+        let currentCivilizationNumber = civGroups[number]
+        let currentArea = civQueue[number].dequeue()!
         let x = currentArea.x
         let y = currentArea.y
         for direction in 0..<xDirections.count {
@@ -83,33 +94,50 @@ func BFS(civilization number: Int) {
             }
             
             // 범위 내지만 동일 문명이면 건너뛴다.
-            let civilizationNumberOfNextArea = findParentCivilization(civilization: civilizationMap[nextX][nextY])
-            if civilizationNumberOfNextArea == number{
+            let civilizationNumberOfNextArea = findParentCivilization(civilization: map[nextX][nextY])
+            if civilizationNumberOfNextArea == currentCivilizationNumber{
                 continue
             }
             
             // 미개한 문명이라면 문명을 퍼뜨리고, 미개하지 않으면서 다른 문명이면 문명을 합치고 해당 문명 큐의것을 내 큐로 가져온다.
             if civilizationNumberOfNextArea == -1 {
-                civilizations[number].enqueue(Area(x: nextX, y: nextY))
-                civilizationMap[nextX][nextY] = number
-            } else {
-                numOfCurrentCivilizations -= 1
-                civilizationsMergedTo[civilizationNumberOfNextArea] = number
-                while !civilizations[civilizationNumberOfNextArea].isEmpty() {
-                    numOfTry += 1
-                    civilizations[number].enqueue(civilizations[civilizationNumberOfNextArea].dequeue()!)
-                }
+                civQueue[number].enqueue(Area(x: nextX, y: nextY))
+                map[nextX][nextY] = currentCivilizationNumber
+                searchAndMerge(number: currentCivilizationNumber, x: nextX, y: nextY)
             }
         }
     }
 }
 
 func findParentCivilization(civilization number: Int) -> Int {
-    if number == -1 || civilizationsMergedTo[number] == number {
+    if number == -1 || civGroups[number] == number {
         return number
     } else {
-        return findParentCivilization(civilization: civilizationsMergedTo[number])
+        return findParentCivilization(civilization: civGroups[number])
     }
 }
 
+func searchAndMerge(number: Int, x: Int, y: Int) {
+    for direction in 0..<xDirections.count {
+        let nextX = x + xDirections[direction]
+        let nextY = y + yDirections[direction]
+        
+        // 범위 밖이면 건너뛴다.
+        if nextX < 1 || nextX > width || nextY < 1 || nextY > width {
+            continue
+        }
+        
+        // 범위 내지만 동일 문명이면 건너뛴다.
+        let civilizationNumberOfNextArea = findParentCivilization(civilization: map[nextX][nextY])
+        if civilizationNumberOfNextArea == number{
+            continue
+        }
+        
+        // 다른 문명이면 문명을 합치고 해당 문명 큐의것을 내 큐로 가져온다.
+        if civilizationNumberOfNextArea != -1 {
+            remainingCivs -= 1
+            civGroups[civilizationNumberOfNextArea] = number
+        }
+    }
+}
 
